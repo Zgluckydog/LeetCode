b085af6b3c49be08efee50c016603e1e47b50e36
Zgluckydog <2505979936@qq.com>
2025-04-01 17:40:57 +0800

    代码随想录学习

--- scripts/code_array.py
+++ scripts/code_array.py
@@ -1,10 +1,5 @@
 from collections import defaultdict
 
-class ListNode:
-    def __init__(self, val,next=None):
-        self.val = val
-        self.next = next
-
 class Solution(object):
     # 704. 两数之和
     def search(self, nums, target):
@@ -307,31 +302,4 @@ class Solution(object):
                 left += 1
         return res
 
-    # 203. 移除链表元素
-    def removeElements(self, head, val):
-        """
-        :type head: Optional[ListNode]
-        :type val: int
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode(0,head)
-        node = dummy
-        while node.next:
-            if node.next.val == val:
-                node.next = node.next.next
-            else:
-                node = node.next
-        return dummy.next
 sol = Solution()
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(6)
-head.next.next.next = ListNode(3)
-head.next.next.next.next = ListNode(4)
-head.next.next.next.next.next = ListNode(5)
-head.next.next.next.next.next.next = ListNode(6)
-val = 6
-# res = sol.removeElements(head, val)
-# while res:
-#     print(res.val)
-#     res = res.next
---
+++ scripts/code_list.py
@@ -0,0 +1,33 @@
+class ListNode:
+    def __init__(self, val,next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    # 203. 移除链表元素
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0,head)
+        node = dummy
+        while node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        return dummy.next
+sol = Solution()
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(6)
+head.next.next.next = ListNode(3)
+head.next.next.next.next = ListNode(4)
+head.next.next.next.next.next = ListNode(5)
+head.next.next.next.next.next.next = ListNode(6)
+val = 6
+res = sol.removeElements(head, val)
+while res:
+    print(res.val)
+    res = res.next
