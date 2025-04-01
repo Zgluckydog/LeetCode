41858456dc4f185ac577c7e8456ad45a0b0a53ec
Zgluckydog <2505979936@qq.com>
2025-04-01 10:12:22 +0800

    代码随想录学习

--- scripts/code.py
+++ scripts/code.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    # 704
+    # 704. 两数之和
     def search(self, nums, target):
         left = 0
         right = len(nums) - 1
@@ -9,7 +9,7 @@ class Solution(object):
             elif nums[mid] < target: left = mid + 1
             elif nums[mid] > target: right = mid - 1
         return -1
-    # 35
+    # 35. 搜索插入位置
     def searchInsert(self, nums, target):
             left = 0
             right = len(nums) - 1
@@ -22,6 +22,7 @@ class Solution(object):
                 elif nums[mid] > target:
                     right = mid - 1
             return left
+    # 34.在排序数组中查找元素的第一个和最后一个位置
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
@@ -55,6 +56,7 @@ class Solution(object):
                 right = mid - 1
 
         return [border_left,border_right]
+    # 69.x 的平方根
     def mySqrt(self, x):
         """
         :type x: int
@@ -71,8 +73,96 @@ class Solution(object):
             else :
                 right = mid - 1
         return res
+    # 367.有效的完全平方数
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1 or num ==0 : return True
+        left = 0
+        right = num - 1
+        while left <= right:
+            mid = left + (right - left)//2
+            if mid*mid == num:
+                return True
+            elif mid*mid < num:
+                left = mid + 1
+            elif mid * mid >num:
+                right = mid - 1
+        return False
+    # 27. 移除元素
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        fastIndex = 0
+        slowIndex = 0
+        while fastIndex < len(nums):
+            if nums[fastIndex] != val:
+                nums[slowIndex] = nums[fastIndex]
+                slowIndex +=1
+            fastIndex +=1
+        return slowIndex
+    # 26. 删除有序数组中的重复项
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        fastIndex = 1
+        slowIndex = 1
+        while fastIndex < len(nums):
+            if nums[fastIndex-1] != nums[fastIndex]:
+                nums[slowIndex] = nums[fastIndex]
+                slowIndex+=1
+            fastIndex+=1
+        return slowIndex
+    # 283. 移动零
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 0 or len(nums) == 1: return nums
+        fastIndex = slowIndex = 0
+        while fastIndex<len(nums):
+            if nums[fastIndex]!=0:
+                nums[slowIndex] = nums[fastIndex]
+                slowIndex+=1
+            fastIndex +=1
+        for i in range(slowIndex,len(nums)):
+            nums[i] = 0
+        return nums
+    # 844.比较含退格的字符串
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        res = []
+        for c in s:
+            if c != '#':
+                res.append(c)
+            elif len(res)!=0:
+                res.pop()
+        s = ''.join(res)
 
+        res = []
+        for c in t:
+            if c != '#':
+                res.append(c)
+            elif len(res)!=0:
+                res.pop()
+        t = ''.join(res)
+        return s == t
+    # 977. 有序数组的平方
 
 sol = Solution()
 
-print(sol.mySqrt(5))
+s = "ab#c"
+t = "ad#c"
+print(sol.backspaceCompare(s, t))
